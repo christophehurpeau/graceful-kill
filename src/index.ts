55d2bab6253b509d0fe4643f907c59df62489f30
Christophe Hurpeau <christophe@hurpeau.com>
2021-12-12 23:53:39 +0100

    refactor: export named

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,25 @@
 import type { ChildProcess } from 'child_process';
-import Logger from 'nightingale-logger';
+import { Logger } from 'nightingale-logger';
 
 const logger = new Logger('graceful-kill');
 
-export default function gracefulKill(
+export function gracefulKill(
   process: ChildProcess,
   SIGTERMTimeout = 4000,
 ): Promise<void> {
   return new Promise((resolve) => {
     if (process.exitCode !== null || process.signalCode !== null) {
       logger.warn('process already exited', { pid: process.pid });
-      return resolve();
+      resolve();
+      return;
     }
     const killTimeout = setTimeout(() => {
       if (process.exitCode !== null || process.signalCode !== null) {
         logger.warn('kill timeout: process already exited', {
           pid: process.pid,
         });
-        return resolve();
+        resolve();
+        return;
       }
 
       logger.warn('kill timeout: sending SIGKILL...', { pid: process.pid });
@@ -33,3 +35,5 @@ export default function gracefulKill(
     process.kill();
   });
 }
+
+export default gracefulKill;
